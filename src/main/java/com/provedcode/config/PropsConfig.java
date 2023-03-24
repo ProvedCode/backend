13a2c9343c79ea0f4a362df43f502e72e256340c
Mykhailo Ordyntsev <102993813+Maslyna@users.noreply.github.com>
2023-03-24 20:47:23 +0100

    user-story-1 (#35)
    * Updated project structure
    
    * Refactored code
    
    * Added profiles

--- src/main/java/com/provedcode/config/PropsConfig.java
+++ src/main/java/com/provedcode/config/PropsConfig.java
@@ -4,6 +4,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-
+@EnableConfigurationProperties(PageProperties.class)
+@PropertySource("pagination.properties")
+@Configuration
 public class PropsConfig {
 }
