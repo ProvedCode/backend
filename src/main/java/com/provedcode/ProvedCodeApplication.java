c75e438a2f880ed707717a8c3fea2be9aaefbaa5
Maslyna <sanci324@gmail.com>
2023-03-24 17:12:16 +0100

    Removed unused library

--- src/main/java/com/provedcode/ProvedCodeApplication.java
+++ src/main/java/com/provedcode/ProvedCodeApplication.java
@@ -1,13 +1,11 @@
 package com.provedcode;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@OpenAPIDefinition
 public class ProvedCodeApplication {
 
 	public static void main(String[] args) {
