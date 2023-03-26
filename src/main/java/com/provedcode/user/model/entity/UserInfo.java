a21a15a1fe9542366b6e1966313ff84a369957e7
Ren <sergeysolovyov2016@gmail.com>
2023-03-27 00:39:02 +0300

    Edit UserInfo: add user_id and @NotNull, @NotEmpty validations

--- src/main/java/com/provedcode/user/model/entity/UserInfo.java
+++ src/main/java/com/provedcode/user/model/entity/UserInfo.java
@@ -2,6 +2,8 @@ package com.provedcode.user.model.entity;
 
 import com.provedcode.talent.model.entity.Talent;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,16 +16,23 @@ import java.util.Set;
 @Entity
 @Table(name = "user_info")
 public class UserInfo {
-    @NotNull
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
+    @NotNull
+    @Column(name = "user_id")
+    private Long userId;
+    @NotEmpty
+    @NotNull
     @Column(name = "login", length = 100)
     private String login;
+    @NotEmpty
+    @NotNull
     @Column(name = "password")
     private String password;
     @OneToOne(orphanRemoval = true)
-    @JoinColumn(name = "id")
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
     private Talent talent;
     @OneToMany(mappedBy = "userInfo", orphanRemoval = true)
     private Set<UserAuthority> userAuthorities = new LinkedHashSet<>();
