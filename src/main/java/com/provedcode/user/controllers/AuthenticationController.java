b386b4e4fe6561ddb4c7db7ac7ae4d403cb94c98
Maslyna <sanci324@gmail.com>
2023-03-27 21:16:38 +0200

    commit 3

--- src/main/java/com/provedcode/user/controllers/AuthenticationController.java
+++ src/main/java/com/provedcode/user/controllers/AuthenticationController.java
@@ -1,21 +1,28 @@
 package com.provedcode.user.controllers;
 
+import com.provedcode.talent.model.entity.Talent;
+import com.provedcode.talent.repo.db.TalentEntityRepository;
+import com.provedcode.user.model.dto.RegistrationDTO;
 import com.provedcode.user.model.entity.UserInfo;
+import com.provedcode.user.repo.UserAuthorityRepository;
 import com.provedcode.user.repo.UserInfoRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.Instant;
-import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static java.time.temporal.ChronoUnit.MINUTES;
@@ -23,12 +30,15 @@ import static java.time.temporal.ChronoUnit.MINUTES;
 @RestController
 @AllArgsConstructor
 @Slf4j
+@RequestMapping("/api")
 public class AuthenticationController {
     JwtEncoder jwtEncoder;
-    UserInfoRepository repository;
+    UserInfoRepository userInfoRepository;
+    TalentEntityRepository talentEntityRepository;
+    UserAuthorityRepository userAuthorityRepository;
     PasswordEncoder passwordEncoder;
 
-    @PostMapping("/api/login")
+    @PostMapping("/login")
     String login(Authentication authentication) {
         log.info("=== POST /login === auth.name = {}", authentication.getName());
         log.info("=== POST /login === auth = {}", authentication);
@@ -41,6 +51,29 @@ public class AuthenticationController {
                 .claim("scope", createScope(authentication))
                 .build();
         return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+    @PostMapping("/register")
+    String register(@Valid RegistrationDTO user) {
+        if (userInfoRepository.existsByLogin(user.login())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, String.format("user with login = {%s} already exists", user.login()));
+        }
+        Talent talent = talentEntityRepository.save(Talent.builder()
+                .firstName(user.firstName())
+                .lastName(user.lastName())
+                .specialization(user.specialization())
+                .build());
+        userInfoRepository.save(
+                UserInfo.builder()
+                        .talent(talent)
+                        .userId(talent.getId())
+                        .userAuthorities(Set.of(userAuthorityRepository.findByAuthority_Authority("ROLE_USER")
+                                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "user didn't created"))))
+                        .login(user.login())
+                        .password(passwordEncoder.encode(user.password()))
+                        .build()
+        );
+        return "YAY";
     }
 
 
