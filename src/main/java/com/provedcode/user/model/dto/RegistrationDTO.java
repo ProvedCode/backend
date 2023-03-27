11b01ccbf3a41465c6dd0e8b2e09c0f10aab180f
Ren <sergeysolovyov2016@gmail.com>
2023-03-28 02:43:17 +0300

    Create trash in AuthenticationController

--- src/main/java/com/provedcode/user/model/dto/RegistrationDTO.java
+++ src/main/java/com/provedcode/user/model/dto/RegistrationDTO.java
@@ -1,5 +1,6 @@
 package com.provedcode.user.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
@@ -7,8 +8,12 @@ import lombok.Builder;
 public record RegistrationDTO(
         @NotEmpty String login,
         @NotEmpty String password,
-        @NotEmpty String firstName,
-        @NotEmpty String lastName,
+        @JsonProperty("first_name")
+        @NotEmpty
+        String firstName,
+        @JsonProperty("last_name")
+        @NotEmpty
+        String lastName,
         @NotEmpty String specialization
 ) {
 }
