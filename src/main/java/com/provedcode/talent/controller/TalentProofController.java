1699d86aaf72f817a0fa97349844bf367d9c1a8a
Ren <sergeysolovyov2016@gmail.com>
2023-04-06 18:04:30 +0300

    Edit TalentProofController

--- src/main/java/com/provedcode/talent/controller/TalentProofController.java
+++ src/main/java/com/provedcode/talent/controller/TalentProofController.java
@@ -4,12 +4,12 @@ import com.provedcode.talent.mapper.TalentProofMapper;
 import com.provedcode.talent.model.dto.ProofDTO;
 import com.provedcode.talent.service.TalentProofService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -20,8 +20,8 @@ public class TalentProofController {
     TalentProofMapper talentProofMapper;
 
     @GetMapping("/proofs")
-    List<ProofDTO> getAllProofs(@RequestParam(value = "page") Optional<Integer> page,
+    Page<ProofDTO> getAllProofs(@RequestParam(value = "page") Optional<Integer> page,
                                 @RequestParam(value = "size") Optional<Integer> size) {
-        return talentProofService.getAllProofsPage(page, size).stream().map(talentProofMapper::toProofDTO).toList();
+        return talentProofService.getAllProofsPage(page, size).map(talentProofMapper::toProofDTO);
     }
 }
