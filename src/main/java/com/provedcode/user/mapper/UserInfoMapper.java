18ab270d1ef2c2a1c5c5e08cdf6d6b19b2177be5
Ren <sergeysolovyov2016@gmail.com>
2023-04-06 14:58:21 +0300

    Create mapper for UserInfo with mapstruct

--- src/main/java/com/provedcode/user/mapper/UserInfoMapper.java
+++ src/main/java/com/provedcode/user/mapper/UserInfoMapper.java
@@ -1,8 +1,22 @@
 package com.provedcode.user.mapper;
 
 import com.provedcode.user.model.entity.UserInfo;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserInfoMapper {
-    UserDetails toUserDetails(UserInfo user);
+    default UserDetails toUserDetails(UserInfo user) {
+        return User.withUsername(user.getLogin())
+                   .password(user.getPassword())
+                   .authorities(user.getAuthorities()
+                                    .stream()
+                                    .map(i -> new SimpleGrantedAuthority(i.getAuthority()))
+                                    .toList())
+                   .build();
+    }
 }
