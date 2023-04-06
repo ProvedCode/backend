3737dbd3dcefa4603c3c34cebe3b6a9acd70a4d8
Ren <sergeysolovyov2016@gmail.com>
2023-04-06 14:58:00 +0300

    Create mappers for Talent and TalentProofs with mapstruct

--- src/main/java/com/provedcode/talent/mapper/TalentMapper.java
+++ src/main/java/com/provedcode/talent/mapper/TalentMapper.java
@@ -2,11 +2,15 @@ package com.provedcode.talent.mapper;
 
 import com.provedcode.talent.model.dto.FullTalentDTO;
 import com.provedcode.talent.model.dto.ShortTalentDTO;
-import com.provedcode.talent.model.entity.*;
+import com.provedcode.talent.model.entity.Talent;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface TalentMapper {
-
     ShortTalentDTO talentToShortTalentDTO(Talent talent);
+
     FullTalentDTO talentToFullTalentDTO(Talent talent);
 
 }
