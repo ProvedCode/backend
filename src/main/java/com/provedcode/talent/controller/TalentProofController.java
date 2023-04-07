17c4454a761f453dd9a89cc77d6fa97beae67980
Maslyna <sanci324@gmail.com>
2023-04-07 17:32:49 +0200

    Created FullProofDTO Added to controller and TalentProofService getTalentProofs method

--- src/main/java/com/provedcode/talent/controller/TalentProofController.java
+++ src/main/java/com/provedcode/talent/controller/TalentProofController.java
@@ -1,14 +1,13 @@
 package com.provedcode.talent.controller;
 
 import com.provedcode.talent.mapper.TalentProofMapper;
+import com.provedcode.talent.model.dto.FullProofDTO;
 import com.provedcode.talent.model.dto.ProofDTO;
 import com.provedcode.talent.service.TalentProofService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -23,5 +22,13 @@ public class TalentProofController {
     Page<ProofDTO> getAllProofs(@RequestParam(value = "page") Optional<Integer> page,
                                 @RequestParam(value = "size") Optional<Integer> size) {
         return talentProofService.getAllProofsPage(page, size).map(talentProofMapper::toProofDTO);
+    }
+
+    @GetMapping("/{talent-id}/proofs")
+    FullProofDTO getTalentInformationWithProofs(@PathVariable("talent-id") Long talentId,
+                                                @RequestParam(value = "page") Optional<Integer> page,
+                                                @RequestParam(value = "size") Optional<Integer> size,
+                                                Authentication authentication) {
+        return talentProofService.getTalentProofs(talentId, page, size, authentication);
     }
 }
--- src/main/java/com/provedcode/talent/service/TalentProofService.java
+++ src/main/java/com/provedcode/talent/service/TalentProofService.java
@@ -2,11 +2,18 @@ package com.provedcode.talent.service;
 
 import com.provedcode.config.PageProperties;
 import com.provedcode.talent.model.ProofStatus;
+import com.provedcode.talent.model.dto.FullProofDTO;
+import com.provedcode.talent.model.dto.ProofDTO;
+import com.provedcode.talent.model.entity.Talent;
 import com.provedcode.talent.model.entity.TalentProof;
 import com.provedcode.talent.repo.TalentProofRepository;
+import com.provedcode.talent.repo.TalentRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -16,8 +23,10 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class TalentProofService {
     TalentProofRepository talentProofRepository;
+    TalentRepository talentRepository;
     PageProperties pageProperties;
 
     public Page<TalentProof> getAllProofsPage(Optional<Integer> page, Optional<Integer> size) {
@@ -28,9 +37,40 @@ public class TalentProofService {
             throw new ResponseStatusException(BAD_REQUEST, "'size' query parameter must be greater than or equal to 1");
         }
         return talentProofRepository.findByStatus(ProofStatus.PUBLISHED,
-                                                  PageRequest.of(page.orElse(
-                                                                         pageProperties.defaultPageNum()),
-                                                                 size.orElse(
-                                                                         pageProperties.defaultPageSize())));
+                PageRequest.of(page.orElse(
+                                pageProperties.defaultPageNum()),
+                        size.orElse(
+                                pageProperties.defaultPageSize())));
+    }
+
+    public FullProofDTO getTalentProofs(Long talentId, Optional<Integer> page, Optional<Integer> size, Authentication authentication) {
+        Talent talent = talentRepository.findById(talentId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Talent with id = %s not found".formatted(talentId)));
+        if (page.orElse(pageProperties.defaultPageNum()) < 0) {
+            throw new ResponseStatusException(BAD_REQUEST, "'page' query parameter must be greater than or equal to 0");
+        }
+        if (size.orElse(pageProperties.defaultPageSize()) <= 0) {
+            throw new ResponseStatusException(BAD_REQUEST, "'size' query parameter must be greater than or equal to 1");
+        }
+        Page<TalentProof> proofs = null;
+        log.info("auth = {}", authentication);
+
+
+                proofs = talentProofRepository.findByTalentId(talentId,
+                PageRequest.of(page.orElse(pageProperties.defaultPageNum()), size.orElse(pageProperties.defaultPageSize())));
+
+        return FullProofDTO.builder()
+                .id(talent.getId())
+                .image(talent.getImage())
+                .firstName(talent.getFirstName())
+                .lastName(talent.getLastName())
+                .specialization(talent.getSpecialization())
+                .proofs(proofs.map(i -> ProofDTO.builder()
+                        .id(i.getId())
+                        .created(i.getCreated().toString())
+                        .link(i.getLink())
+                        .text(i.getText())
+                        .status(i.getStatus()).build()))
+                .build();
     }
 }
