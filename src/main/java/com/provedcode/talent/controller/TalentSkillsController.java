d24784e17984054ec77b2dffe553f1b9f599048d
Denis973 <46610112+Denis973@users.noreply.github.com>
2023-05-07 14:27:21 +0300

    S4.1 tasks 4 5 (#119)
    * add getAllSkillsOnProof
    
    * add deleteSkillOnProof

--- src/main/java/com/provedcode/talent/controller/TalentSkillsController.java
+++ src/main/java/com/provedcode/talent/controller/TalentSkillsController.java
@@ -1,10 +1,12 @@
 package com.provedcode.talent.controller;
 
 import com.provedcode.talent.model.dto.ProofSkillsDTO;
+import com.provedcode.talent.model.dto.SkillsOnProofDTO;
 import com.provedcode.talent.service.TalentSkillsService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -24,5 +26,20 @@ public class TalentSkillsController {
                          @RequestBody @Valid ProofSkillsDTO skills,
                          Authentication authentication) {
         talentSkillsService.addSkillsOnProof(talentId, proofId, skills, authentication);
+    }
+
+    @GetMapping("/{talent-id}/proofs/{proof-id}/skills")
+    SkillsOnProofDTO getAllSkillsOnProof(@PathVariable("talent-id") long talentId,
+                                         @PathVariable("proof-id") long proofId,
+                                         Authentication authentication) {
+        return talentSkillsService.getAllSkillsOnProof(talentId, proofId, authentication);
+    }
+    @PreAuthorize("hasRole('TALENT')")
+    @DeleteMapping("/{talent-id}/proofs/{proof-id}/skills/{skill-id}")
+    void deleteSkillOnProof(@PathVariable("talent-id") long talentId,
+                            @PathVariable("proof-id") long proofId,
+                            @PathVariable("skill-id") long skillId,
+                            Authentication authentication) {
+        talentSkillsService.deleteSkillOnProof(talentId, proofId, skillId, authentication);
     }
 }
--- src/main/java/com/provedcode/talent/service/TalentSkillsService.java
+++ src/main/java/com/provedcode/talent/service/TalentSkillsService.java
@@ -2,7 +2,9 @@ package com.provedcode.talent.service;
 
 import com.provedcode.talent.model.ProofStatus;
 import com.provedcode.talent.model.dto.ProofSkillsDTO;
+import com.provedcode.talent.model.dto.SkillsOnProofDTO;
 import com.provedcode.talent.model.entity.Skills;
+import com.provedcode.talent.model.entity.Talent;
 import com.provedcode.talent.model.entity.TalentProof;
 import com.provedcode.talent.repo.SkillsRepository;
 import com.provedcode.talent.repo.TalentProofRepository;
@@ -16,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.function.BiConsumer;
 
@@ -60,5 +61,57 @@ public class TalentSkillsService {
         talentProofRepository.save(talentProof);
     }
 
+    @Transactional(readOnly = true)
+    public SkillsOnProofDTO getAllSkillsOnProof(long talentId, long proofId, Authentication authentication) {
+        TalentProof talentProof = talentProofRepository.findById(proofId)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND,
+                        "proof with id = %s not found".formatted(proofId)));
+        Talent talent = talentRepository.findById(talentId)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND,
+                        "talent with id = %s not found".formatted(talentId)));
+        if (!talent.getId().equals(talentProof.getTalent().getId())) {
+            throw new ResponseStatusException(BAD_REQUEST,
+                    "talentId with id = %s and proofId with id = %s do not match".formatted(talentId, proofId));
+        }
 
+        if (talentProof.getStatus().equals(ProofStatus.PUBLISHED)) {
+            return SkillsOnProofDTO.builder().skills(talentProof.getSkills()).build();
+        } else if (authentication != null) {
+            UserInfo userInfo = userInfoRepository.findByLogin(authentication.getName())
+                    .orElseThrow(() -> new ResponseStatusException(NOT_FOUND));
+            if (userInfo.getTalent().getId().equals(talentProof.getTalent().getId())) {
+                return SkillsOnProofDTO.builder().skills(talentProof.getSkills()).build();
+            } else {
+                throw new ResponseStatusException(FORBIDDEN, "you can't see proofs in DRAFT and HIDDEN status");
+            }
+        } else {
+            throw new ResponseStatusException(FORBIDDEN, "you can't see proofs in DRAFT and HIDDEN status");
+        }
+    }
+
+    public void deleteSkillOnProof(long talentId, long proofId, long skillId, Authentication authentication) {
+        Talent talent = talentRepository.findById(talentId)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND,
+                        "talent with id = %s not found".formatted(talentId)));
+        UserInfo userInfo = userInfoRepository.findByLogin(authentication.getName())
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND));
+        TalentProof talentProof = talentProofRepository.findById(proofId)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "proof with id = %s not found".formatted(proofId)));
+        if (!talentProof.getStatus().equals(ProofStatus.DRAFT)) {
+            throw new ResponseStatusException(CONFLICT, "proof status must be DRAFT");
+        }
+        if (!talent.getId().equals(talentProof.getTalent().getId())) {
+            throw new ResponseStatusException(BAD_REQUEST,
+                    "talentId with id = %s and proofId with id = %s do not match".formatted(talentId, proofId));
+        }
+        isValidUserEditTalent.accept(talentId, userInfo);
+        Skills skills = skillsRepository.findById(skillId)
+                .orElseThrow(() -> new ResponseStatusException(NOT_FOUND,
+                        "skill with id = %s not found".formatted(skillId)));
+        if (!talentProof.getSkills().contains(skills)) {
+            throw new ResponseStatusException(NOT_FOUND,
+                    "you dont have skill with id = %s on proof with id = %s".formatted(skillId, proofId));
+        }
+        talentProof.getSkills().remove(skills);
+    }
 }
