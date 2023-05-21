e4a3980a53a8472f0f6b0e37140a7b8f550507ce
Maslyna <sanci324@gmail.com>
2023-05-21 21:32:47 +0200

    AWS minore update

--- src/main/java/com/provedcode/kudos/controller/KudosController.java
+++ src/main/java/com/provedcode/kudos/controller/KudosController.java
@@ -14,7 +14,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
